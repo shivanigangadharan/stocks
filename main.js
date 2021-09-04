53ccef8512761f44a852e620a944d489ae614f6f
shivanigangadharan <shivani.gangadharan@gmail.com>
2021-09-04 23:36:27 +0530

    feat: profit/loss function done

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ var oldPrice;
 var noOfStocks = document.getElementById('noOfStocks');
 var newPrice;
 const submitBtn = document.getElementById('submitBtn');
+var result = document.getElementById('result');
 
 function handleChangeO(e) {
     oldPrice = parseInt(e);
@@ -11,8 +12,10 @@ function handleChangeN(p) {
     newPrice = parseInt(p);
 }
 
-function showStatus() {
+function showStatus(event) {
+    event.preventDefault();
     console.log('You made a', oldPrice > newPrice ? 'loss' : 'profit', 'of Rs.', Math.abs((oldPrice * noOfStocks.value) - (newPrice * noOfStocks.value)));
+    result.innerText = ("You made a ") + (oldPrice > newPrice ? ' loss ' : 'profit ') + (' of Rs. ') + (Math.abs((oldPrice * noOfStocks.value) - (newPrice * noOfStocks.value)));
 }
 
 submitBtn.addEventListener('click', showStatus);
