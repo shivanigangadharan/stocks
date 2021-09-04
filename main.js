69cb6c0e8e354d2fb84e22e94b26fe722ca33b74
shivanigangadharan <shivani.gangadharan@gmail.com>
2021-09-04 23:41:54 +0530

    feat: percentage cal done

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ var noOfStocks = document.getElementById('noOfStocks');
 var newPrice;
 const submitBtn = document.getElementById('submitBtn');
 var result = document.getElementById('result');
+var percentage = document.getElementById('percentage');
 
 function handleChangeO(e) {
     oldPrice = parseInt(e);
@@ -16,6 +17,7 @@ function showStatus(event) {
     event.preventDefault();
     console.log('You made a', oldPrice > newPrice ? 'loss' : 'profit', 'of Rs.', Math.abs((oldPrice * noOfStocks.value) - (newPrice * noOfStocks.value)));
     result.innerText = ("You made a ") + (oldPrice > newPrice ? ' loss ' : 'profit ') + (' of Rs. ') + (Math.abs((oldPrice * noOfStocks.value) - (newPrice * noOfStocks.value)));
+    percentage.innerText = ("The ") + (oldPrice > newPrice ? ' loss ' : 'profit ') + ("percentage is ") + (((Math.abs((oldPrice * noOfStocks.value) - (newPrice * noOfStocks.value))) / oldPrice) * 100) + ("%");
 }
 
 submitBtn.addEventListener('click', showStatus);
