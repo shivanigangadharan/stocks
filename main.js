8ec5f7ab279729c682c33064608191988ae0e955
shivanigangadharan <shivani.gangadharan@gmail.com>
2021-09-04 23:22:41 +0530

    feat: main calculation done

--- main.js
+++ main.js
@@ -1,25 +1,18 @@
-const oldPrice = document.getElementById('oldPrice');
-const noOfStocks = document.getElementById('noOfStocks');
-const newPrice = document.getElementById('newPrice');
-
-
+var oldPrice;
+var noOfStocks = document.getElementById('noOfStocks');
+var newPrice;
 const submitBtn = document.getElementById('submitBtn');
 
-
-function showStatus() {
-    var status;
-    if (oldPrice.value > newPrice.value) {
-        status = 'loss';
-    }
-    else if (oldPrice.value < newPrice.value) {
-        status = 'profit';
-    }
-    else {
-        status = 'same';
-    }
-    console.log('You made a', status, 'of Rs.', Math.abs((oldPrice.value * noOfStocks.value) - (newPrice.value * noOfStocks.value)));
-
+function handleChangeO(e) {
+    oldPrice = parseInt(e);
 }
 
+function handleChangeN(p) {
+    newPrice = parseInt(p);
+}
+
+function showStatus() {
+    console.log('You made a', oldPrice > newPrice ? 'loss' : 'profit', 'of Rs.', Math.abs((oldPrice * noOfStocks.value) - (newPrice * noOfStocks.value)));
+}
 
 submitBtn.addEventListener('click', showStatus);
