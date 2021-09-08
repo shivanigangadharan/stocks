d96665757e80ce8285fd3e2375ed4ef6bb4e26a0
shivanigangadharan <shivani.gangadharan@gmail.com>
2021-09-09 02:53:05 +0530

    style: css done+corner cases

--- main.js
+++ main.js
@@ -15,6 +15,14 @@ function handleChangeN(p) {
 
 function showStatus(event) {
     event.preventDefault();
+    if (oldPrice == undefined || newPrice == undefined || noOfStocks == undefined) {
+        alert('Please enter values for all 3 inputs.');
+        return 0;
+    }
+    if (oldPrice < 0 || newPrice < 0 || noOfStocks < 0) {
+        alert('Please enter valid inputs.');
+        return 0;
+    }
     console.log('You made a', oldPrice > newPrice ? 'loss' : 'profit', 'of Rs.', Math.abs((oldPrice * noOfStocks.value) - (newPrice * noOfStocks.value)));
     result.innerText = ("You made a ") + (oldPrice > newPrice ? ' loss ' : 'profit ') + (' of Rs. ') + (Math.abs((oldPrice * noOfStocks.value) - (newPrice * noOfStocks.value)));
     percentage.innerText = ("The ") + (oldPrice > newPrice ? ' loss ' : 'profit ') + ("percentage is ") + (((Math.abs((oldPrice * noOfStocks.value) - (newPrice * noOfStocks.value))) / oldPrice) * 100) + ("%");
